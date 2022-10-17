49233cc4d23c7873063fb26ff8416c3c31a5bf7f
Hamid <54853635+hamiboy8260@users.noreply.github.com>
2022-10-17 14:02:12 +0200

    Fix: Homepage Posts

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,26 +1,32 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSearchbar } from "@ionic/react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonSearchbar,
+} from "@ionic/react";
 import { useState } from "react";
 import ProductCard from "../components/ProductCard";
 
 export default function SearchPage() {
+  const [searchText, setSearchText] = useState("");
 
-  const [searchText, setSearchText] = useState('');
-  
   // Create array of objects
   const items = [
-    { title: 'Apple', type: 'Fruit' },
-    { title: 'Banana', type: 'Fruit' },
-    { title: 'Carrot', type: 'Vegetable' },
-    { title: 'Potato', type: 'Vegetable' },
+    { title: "Apple", type: "Fruit" },
+    { title: "Banana", type: "Fruit" },
+    { title: "Carrot", type: "Vegetable" },
+    { title: "Potato", type: "Vegetable" },
   ];
 
-
   // Filter array of objects
-  const filteredItems = items.filter(item => item.title.toLowerCase().includes(searchText.toLowerCase()));
+  const filteredItems = items.filter((item) =>
+    item.title.toLowerCase().includes(searchText.toLowerCase())
+  );
 
   return (
     <IonPage>
-    
       <IonHeader>
         <IonToolbar>
           <IonTitle>Search</IonTitle>
@@ -32,12 +38,16 @@ export default function SearchPage() {
             <IonTitle size="large">Search</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonSearchbar autocorrect = "on" spellcheck = {true} showClearButton="focus" value={searchText} onIonChange={(e)=>setSearchText(e.detail.value)}></IonSearchbar>
+        <IonSearchbar
+          autocorrect="on"
+          spellcheck={true}
+          showClearButton="focus"
+          value={searchText}
+          onIonChange={(e) => setSearchText(e.detail.value)}
+        ></IonSearchbar>
         {filteredItems.map((item, index) => (
-           <ProductCard product={item} key={index} />
-          
+          <ProductCard product={item} key={index} />
         ))}
-        
       </IonContent>
     </IonPage>
   );
